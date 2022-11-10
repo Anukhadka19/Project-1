ea53b06720ebd524e27c2169f1a33a0efa21f2d9
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2022-11-10 17:45:59 -0500

    updated html and styles

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+// const drinkType = ['vodka', 'rum', 'gin', 'whiskey'];
+// const mealType = ['breakfast', 'vegetarian', 'seafood', 'dessert'];
+
+const removeItemBtnCollection = document.getElementsByClassName('remove-item-btn');
+const btnArray = Array.from(removeItemBtnCollection);
+
+// Need to make sure the 'category' passed in matches exactly one/any of the potential user inputs -- meaning the 'select' options should be exactly the same
+function grabDrinkArray (category) {
+    const drinkFetchUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${category}&api_key=1`;
+    fetch(drinkFetchUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            if (data) {
+                const drinkArray = []
+                for (let i = 0; i < 6; i++) {
+                    const drinkObject = {name: `${data.drinks[i].strDrink}`,
+                                        imgUrl: `${data.drinks[i].strDrinkThumb}`
+                                    }
+                    drinkArray.push(drinkObject);
+                }
+                // The function that builds the elements based on this array goes here
+            }
+        })
+        // Will want to do more (eventually) than just logging the error message
+        .catch(function (error) {
+            console.log(error.message);
+        })
+}
+
+// Need to make sure the 'category' passed in matches exactly one/any of the potential user inputs -- meaning the 'select' options should be exactly the same
+function grabFoodArray (category) {
+    const mealFetchUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}&api_key=1`;
+    fetch(mealFetchUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            if (data) {
+                const mealArray = []
+                for (let i = 0; i < 6; i++) {
+                    const mealObject = {name: `${data.meals[i].strMeal}`,
+                                        imgUrl: `${data.meals[i].strMealThumb}`
+                                    }
+                    mealArray.push(mealObject);
+                }
+                // The function that builds the elements based on this array goes here
+            }
+        })
+        // Will want to do more (eventually) than just logging the error message
+        .catch(function (error) {
+            console.log(error.message);
+        });
+}
